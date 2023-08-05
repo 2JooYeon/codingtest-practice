beafe6925d5e7393c809598587031425200f51a5
2JooYeon <lewndus7749@naver.com>
2023-08-06 02:56:05 +0900

    success

--- 이것이 취업을 위한 코딩테스트다 with 파이썬/곱하기 혹은 더하기.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬/곱하기 혹은 더하기.py
@@ -1,15 +1,10 @@
 s = list(map(int, list(input())))
-answer = 0
-eval_flag = 0
+answer = s[0]
 
-if len(s) == 1:
-    answer = s[0]
-
-else:
-    for n in s:
-        if n == 0 or answer == 0:
-            answer += n
-        else:
-            answer *= n
+for n in s[1:]:
+    if answer <= 1 or n <= 1:
+        answer += n
+    else:
+        answer *= n
 
 print(answer)
