30b2b779ac73778f0ceb375325bf8e6a820db4f6
2JooYeon <lewndus7749@naver.com>
2023-10-14 21:30:03 +0900

    상어 초등학교_success(코드 수정)

--- Baekjoon/21608.py
+++ Baekjoon/21608.py
@@ -11,8 +11,7 @@ dy = [0, 0, -1, 1]
 
 # 문제 조건에 맞춰 앉을 자리를 찾는 함수
 def find_seat(student_id):
-    fav = 0
-    blank = 0
+    fav_list = []
     for x in range(n):
         for y in range(n):
             temp_fav, temp_blank = 0, 0
@@ -26,16 +25,10 @@ def find_seat(student_id):
                     temp_fav += 1
                 if graph[nx][ny] == 0:
                     temp_blank += 1
-            if temp_fav > fav:
-                sx, sy = x, y
-                fav = temp_fav
-                blank = temp_blank
-            elif temp_fav == fav and temp_blank > blank:
-                sx, sy = x, y
-                blank = temp_blank
-            # 주변에 좋아하는 학생도 0명이고, 빈칸도 0개일 때
-            elif temp_fav == fav and temp_blank == blank:
-                sx, sy = x, y
+            fav_list.append([temp_fav, temp_blank, x, y])
+    '''문제 조건대로 우선순위를 고려해서 정렬'''
+    fav_list.sort(key=lambda x:(-x[0], -x[1], x[2], x[3]))
+    _, _, sx, sy = fav_list[0]
     return sx, sy
 
 
