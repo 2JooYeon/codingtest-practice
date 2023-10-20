5cfae15eda2c765ebb9e016c38cafe17ffd2a448
2JooYeon <lewndus7749@naver.com>
2023-10-20 18:36:24 +0900

    오큰수_success:재풀이

--- Baekjoon/17928.py
+++ Baekjoon/17928.py
@@ -1,14 +1,26 @@
+# n = int(input())
+# data = list(map(int, input().split()))
+# answer = [-1] * n
+# stack = []
+#
+# for i in range(n):
+#     # 스택에 있는 수보다 더 큰 수를 만난 경우
+#     while stack and data[stack[-1]] < data[i]:
+#         # 오큰수로 저장
+#         answer[stack.pop()] = data[i]
+#     # 스택이 비어있거나, 자신보다 더 큰 수를 만나지 못한 경우
+#     stack.append(i)
+#
+# print(' '.join(map(str, answer)))
+
 n = int(input())
 data = list(map(int, input().split()))
-answer = [-1] * n
 stack = []
+answer = [-1] * n
 
 for i in range(n):
-    # 스택에 있는 수보다 더 큰 수를 만난 경우
-    while stack and data[stack[-1]] < data[i]:
-        # 오큰수로 저장
+    while stack and data[i] > data[stack[-1]]:
         answer[stack.pop()] = data[i]
-    # 스택이 비어있거나, 자신보다 더 큰 수를 만나지 못한 경우
     stack.append(i)
 
 print(' '.join(map(str, answer)))
