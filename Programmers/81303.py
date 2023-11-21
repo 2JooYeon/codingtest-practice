98736d91beb9c1479c26fcef3cce798e793737e6
2JooYeon <lewndus7749@naver.com>
2023-11-22 01:33:13 +0900

    표 편집_success(heapq 사용으로 효율성 통과)

--- Programmers/81303.py
+++ Programmers/81303.py
@@ -1,12 +1,16 @@
 '''선택된 번호를 기준으로 left, right 큐로 나눠서 풀이'''
-from collections import deque
+# 효율성을 통과하기 위해 heapq 사용
+from heapq import heappush, heappop
 
 def solution(n, k, cmd):
     answer = ['O'] * n
-    data = list(range(0, n))
-    # 처음에 k행을 선택했으므로 k를 기준으로 left, right 큐로 나누기
-    left = deque(data[:k+1])
-    right = deque(data[k+1:])
+    left = []
+    right = []
+    # 처음에 k행을 선택했으므로 k를 기준으로 left는 최대힙, right는 최소힙으로 관리하기
+    for i in range(k+1):
+        heappush(left, -i)
+    for i in range(k+1, n):
+        heappush(right, i)
     # 삭제된 행을 저장하는 리스트
     delete = []
     for req in cmd:
@@ -15,24 +19,23 @@ def solution(n, k, cmd):
         if oper == 'D':
             num = int(req[1])
             for _ in range(num):
-                left.append(right.popleft())
-        elif oper == 'C':
-            delete.append(left.pop())
-            # 삭제된 행이 가장 마지막 행인 경우 고려
-            if right:
-                left.append(right.popleft())
+                heappush(left, -heappop(right))
         elif oper == 'U':
             num = int(req[1])
             for _ in range(num):
-                right.appendleft(left.pop())
+                heappush(right, -heappop(left))
+        elif oper == 'C':
+            delete.append(-heappop(left))
+            # 삭제된 행이 가장 마지막 행인 경우 고려
+            if right:
+                heappush(left, -heappop(right))
         elif oper == 'Z':
             num = delete.pop()
-            if num > left[-1]:
-                right.append(num)
-                right = deque(sorted(right))
+            if num > -left[0]:
+                heappush(right, num)
             else:
-                left.append(num)
-                left = deque(sorted(left))
+                heappush(left, -num)
+
     for num in delete:
         answer[num] = 'X'
     answer = ''.join(answer)
