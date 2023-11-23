b1ad5c564ce5b44c485e8e9d7f1eac89f9e6837b
2JooYeon <lewndus7749@naver.com>
2023-11-24 01:54:11 +0900

    튜플_success(set을 이용한 풀이)

--- Programmers/64065.py
+++ Programmers/64065.py
@@ -1,4 +1,4 @@
-'''list의 count함수를 이용한 풀이'''
+'''풀이1: list의 count함수를 이용한 풀이'''
 def solution(s):
     answer = []
     s = s.replace('{', '').replace('}', '').split(',')
@@ -11,5 +11,21 @@ def solution(s):
     # 가장 많이 등장한 원소부터 answer에 추가
     for i in range(len(set_s), 0, -1):
         answer.append(num_elem[i])
+
+    return answer
+
+'''풀이2: set을 이용한 풀이 (풀이1 보다 시간효율적)'''
+def solution(s):
+    answer = []
+    tuple_group = s[2:-2].split('},{')
+    tuple_arr = []
+    for group in tuple_group:
+        tuple_arr.append(set(group.split(',')))
+    tuple_arr.sort(key=lambda x: len(x))
+
+    target = set()
+    for arr in tuple_arr:
+        answer.append(int(list(arr - target)[0]))
+        target = arr
 
     return answer
