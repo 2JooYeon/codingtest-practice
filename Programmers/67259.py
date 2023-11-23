3e31654522216628e4e7d645c4ac7b5645f34d36
2JooYeon <lewndus7749@naver.com>
2023-11-23 21:13:36 +0900

    경주로 건설_success

--- Programmers/67259.py
+++ Programmers/67259.py
@@ -1,51 +1,46 @@
-'''
-이 코드의 반례 (현재 코드는 (x, y)에 도착한 순간만 보고 값을 비교하기 때문에 틀렸음)
-[[0,0,0,0,0,0,0,0,0,1],
-[0,1,1,1,1,1,1,1,0,1],
-[0,1,1,1,1,1,1,1,0,1],
-[0,1,1,1,1,1,1,0,0,1],
-[0,1,1,1,1,1,1,0,1,1],
-[0,1,1,1,1,1,1,0,1,1],
-[0,1,1,1,1,0,0,0,0,0],
-[0,1,1,1,1,0,1,1,1,0],
-[0,0,0,0,0,0,1,1,1,0],
-[1,1,1,1,1,1,1,1,1,0]]
-정답: 4200, 오답: 4500
-'''
+'''bfs를 이용해서 최소 비용 찾는 풀'''
 from collections import deque
+
 def solution(board):
     INF = int(1e9)
-    answer = INF
-    # 상우하좌
+    answer = []
+    # 상우하좌 (상하 방향은 인덱스가 짝수, 좌우 방향은 인덱스가 홀수)
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
-    q = deque()
-    n = len(board)
-    visited = [[INF] * n for _ in range(n)]
-    visited[0][0] = 0
-    visited[0][1] = 100
-    visited[1][0] = 100
-    q.append((0, 1, 100, 1))
-    q.append((1, 0, 100, 0))
 
-    while q:
-        x, y, cost, d = q.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+    # (x, y)까지 가는 비용 cost, d:0은 상하, d:1은 좌우
+    def bfs(x,y,cost,d):
+        q = deque()
+        n = len(board)
+        distance = [[INF]*n for _ in range(n)]
+        distance[0][0] = 0
+        q.append((x,y,cost,d))
+
+        while q:
+            x, y, cost, d = q.popleft()
+            # 도착점에 도착한 경우 answer에 추가
+            if x==n-1 and y==n-1:
+                answer.append(cost)
                 continue
-            nd = 0
-            if i % 2:
-                nd = 1
-            if board[nx][ny] == 0:
-                if d == nd:
-                    if cost + 100 <= visited[nx][ny] + 500:
-                        visited[nx][ny] = cost + 100
-                        q.append((nx, ny, cost + 100, nd))
-                else:
-                    if cost + 600 <= visited[nx][ny] + 500:
-                        visited[nx][ny] = cost + 600
-                        q.append((nx, ny, cost + 600, nd))
-    answer = visited[n - 1][n - 1]
+            for i in range(4):
+                nx, ny = x+dx[i], y+dy[i]
+                if nx<0 or nx>=n or ny<0 or ny>=n:
+                    continue
+                # 이동 후 방향 설정
+                nd = 0
+                if i % 2:
+                    nd = 1
+                # 이동 비용 설정
+                ncost = cost+100
+                if d!=nd:
+                    ncost = cost+600
 
-    return answer
+                if board[nx][ny] == 0:
+                    # 기존보다 적은 비용으로 nx, ny에 갈 수 있는 경우만 큐에 추가
+                    if ncost<distance[nx][ny]:
+                        distance[nx][ny] = ncost
+                        q.append((nx, ny, ncost, nd))
+    bfs(0,0,0,0)
+    bfs(0,0,0,1)
+
+    return min(answer)
