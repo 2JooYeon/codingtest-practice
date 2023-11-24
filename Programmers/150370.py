fecf89135ea0285dbcb3f4fdf1ac3ff415eefd90
2JooYeon <lewndus7749@naver.com>
2023-11-24 18:11:22 +0900

    개인정보 수집 유효기간_success(간단하게 재풀이)

--- Programmers/150370.py
+++ Programmers/150370.py
@@ -1,3 +1,4 @@
+'''풀이 1: YYYY.MM.DD 형태로 비교 '''
 def solution(today, terms, privacies):
     answer = []
     # 약관 별 유효기간 저장
@@ -28,6 +29,34 @@ def solution(today, terms, privacies):
         elif day[0] == today[0] and day[1] == today[1] and day[2] > today[2]: continue
 
         answer.append(i+1)
-        answer.sort()
+
+    return answer
+
+
+'''풀이 2: 2000년 1월 1일부터 경과된 일 수를 계산하여 비교'''
+def solution(today, terms, privacies):
+    answer = []
+    # 약관 별 유효기간 저장
+    term_period = {}
+    for term in terms:
+        c, m = term.split()
+        term_period[c] = int(m)
+
+    # 날짜를 숫자 리스트로 변환
+    year, month, day = map(int, today.split('.'))
+    # 2000년 1월 1일 기준으로 오늘까지 며칠이 흘렀는지 계산
+    # 문제에서 한 달은 28일이라고 했으므로 1년은 336일, 1달은 28일
+    today = (year - 2000) * 336 + (month - 1) * 28 + (day - 1)
+
+    n = len(privacies)
+    for i in range(n):
+        privacy = privacies[i]
+        year, month, day = map(int, privacy[:-2].split('.'))
+        # 약관 종류에 해당 하는 유효기간 설정
+        period = term_period[privacy[-1]]
+        # 2000년 1월 1일 기준으로 유효기간까지 며칠이 흘렀는지 계산
+        exday = (year - 2000) * 336 + (month + period - 1) * 28 + (day - 1)
+        if exday <= today:
+            answer.append(i + 1)
 
     return answer
